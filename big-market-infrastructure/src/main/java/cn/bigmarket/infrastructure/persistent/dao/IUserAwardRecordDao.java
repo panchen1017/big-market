000c551660217beca14959ae0e05d46bc7ef1dd5
weixin_54138735 <weixin_54138735@noreply.gitcode.com>
2024-11-28 21:55:03 +0800

    feat:第19节：写入中奖记录和任务补偿发送MQ

--- big-market-infrastructure/src/main/java/cn/bigmarket/infrastructure/persistent/dao/IUserAwardRecordDao.java
+++ big-market-infrastructure/src/main/java/cn/bigmarket/infrastructure/persistent/dao/IUserAwardRecordDao.java
@@ -1,9 +1,16 @@
 package cn.bigmarket.infrastructure.persistent.dao;
 
+import cn.bigmarket.infrastructure.persistent.po.UserAwardRecord;
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
  * @description 用户中奖记录表
  * @create 2024-04-03 15:57
  */
+@Mapper
+@DBRouterStrategy(splitTable = true)
 public interface IUserAwardRecordDao {
+    void insert(UserAwardRecord userAwardRecord);
 }
---
+++ big-market-infrastructure/src/main/java/cn/bigmarket/infrastructure/persistent/repository/TaskRepository.java
@@ -0,0 +1,65 @@
+package cn.bigmarket.infrastructure.persistent.repository;
+
+import cn.bigmarket.domain.task.model.entity.TaskEntity;
+import cn.bigmarket.domain.task.repository.ITaskRepository;
+import cn.bigmarket.infrastructure.event.EventPublisher;
+import cn.bigmarket.infrastructure.persistent.dao.ITaskDao;
+import cn.bigmarket.infrastructure.persistent.po.Task;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 任务服务仓储实现
+ * @create 2024-04-06 10:57
+ */
+@Repository
+public class TaskRepository implements ITaskRepository {
+
+    @Resource
+    private ITaskDao taskDao;
+    @Resource
+    private EventPublisher eventPublisher;
+
+    @Override
+    public List<TaskEntity> queryNoSendMessageTaskList() {
+        List<Task> tasks = taskDao.queryNoSendMessageTaskList();
+        List<TaskEntity> taskEntities = new ArrayList<>(tasks.size());
+        for (Task task : tasks) {
+            TaskEntity taskEntity = new TaskEntity();
+            taskEntity.setUserId(task.getUserId());
+            taskEntity.setTopic(task.getTopic());
+            taskEntity.setMessageId(task.getMessageId());
+            taskEntity.setMessage(task.getMessage());
+            taskEntities.add(taskEntity);
+        }
+        return taskEntities;
+    }
+
+    @Override
+    public void sendMessage(TaskEntity taskEntity) {
+        // 消息发送
+        eventPublisher.publish(taskEntity.getTopic(), taskEntity.getMessage());
+    }
+
+    @Override
+    public void updateTaskSendMessageCompleted(String userId, String messageId) {
+        Task taskReq = new Task();
+        taskReq.setUserId(userId);
+        taskReq.setMessageId(messageId);
+        taskDao.updateTaskSendMessageCompleted(taskReq);
+    }
+
+    @Override
+    public void updateTaskSendMessageFail(String userId, String messageId) {
+        // 发送失败
+        Task taskReq = new Task();
+        taskReq.setUserId(userId);
+        taskReq.setMessageId(messageId);
+        taskDao.updateTaskSendMessageFail(taskReq);
+    }
+
+}
